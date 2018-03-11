ac25bb58b59675f60e1d161ed2a01e1feb167db9
kjaspreet <jaspreet.mca2011@gmail.com>
2018-03-10 22:23:38 -0800

    Updates

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import Home from './Components/Home';
-import registerServiceWorker from './registerServiceWorker';
-
-
-ReactDOM.render(<Home />, document.getElementById('root'));
-registerServiceWorker();
